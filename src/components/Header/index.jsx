a06707863431992d32357c538faced3ebc1adea3
Happy-little-angel <2694913930@qq.com>
2020-06-02 23:40:57 +0800

    homepage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Row,Col} from 'antd'
+import axios from 'axios'
 import './index.less'
 import fromDate from './../../utils/formDate'
 export default class Header extends Component {
@@ -13,10 +14,23 @@ export default class Header extends Component {
             let sysTime = fromDate.formDate(new Date().getTime());
             this.setState({sysTime})
         })
-        // this.getWeatherAPIDATA()
+         this.getWeatherAPIDATA()
     }
     //获得天气
+    getWeatherAPIDATA(){
+        axios.get('/api')
+        .then((res) => {
+                console.log("res:",res);
+                let data = res.data;
+                if (data.message === "success"){
+                    this.setState({
+                        city:data.result.location.city,
+                        weather:data.result.now.text,
+                    })
+                }
 
+        })
+    }
     render() {
         return (
             <div className='header'>
@@ -30,7 +44,11 @@ export default class Header extends Component {
                     <Col span={4} className="breadcrumb-title">首页</Col>
                     <Col span={20} className="weather">
                         <span className="date">{this.state.sysTime}</span>
-                        <span className="weather-detail">晴转多云</span>
+                        <span className="weather-detail">
+                            城市：{this.state.city}
+                            天气：{this.state.weather}
+
+                        </span>
                     </Col>
                 </Row>
             </div>
--- src/utils/formDate.js
+++ src/utils/formDate.js
@@ -2,6 +2,6 @@ export default {
     formDate (time){
         if(!time) return ""
         let date = new Date(time)
-        return date.getFullYear() + "-" +(date.getMonth()+1) + "-" +date.getDate() + " "+date.getHours()+":"+date.getMinutes()+":" +date.getSeconds()
+        return date.getFullYear() + "-" +(date.getMonth()+1) + "-" +date.getDate() + " "+date.getHours()+":"+date.getMinutes()+":" +(date.getSeconds()<=9?"0"+date.getSeconds():date.getSeconds())
     }
 }
