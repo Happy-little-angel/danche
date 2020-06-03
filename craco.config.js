cbf2541e708677d8d2a162e8921e7ddfe4df701e
Happy-little-angel <2694913930@qq.com>
2020-06-03 09:17:23 +0800

    replace before

--- craco.config.js
+++ craco.config.js
@@ -1,19 +1,5 @@
 const CracoLessPlugin = require('craco-less');
 module.exports = {
-    devServer: {
-      port: 3000, // 端口配置
-      proxy: {
-          '/api': {
-              target: ' http://api.map.baidu.com/weather/v1/?district_id=110100&data_type=all&ak=AjgOd5cXAYtLNL2omrQq3OXXxC6IbYw5',
-              ws: false, // websocket
-              changeOrigin: true, //是否跨域
-              secure: false,  // 如果是https接口，需要配置这个参数
-              pathRewrite: {
-                  '^/api': ''
-              }
-          }
-      }
-  },
   plugins: [
     {
       plugin: CracoLessPlugin,
